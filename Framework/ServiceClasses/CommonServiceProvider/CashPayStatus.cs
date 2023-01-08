bd0649e13bdb4345f1df2fd23e170a4c19fec3c6
Claudio Santacesaria <claudio.santacesaria@rototype.com>
2023-01-08 01:01:44 +0100

    CashPay 2

--- Framework/ServiceClasses/CommonServiceProvider/CashPayStatus.cs
+++ Framework/ServiceClasses/CommonServiceProvider/CashPayStatus.cs
@@ -8,6 +8,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,21 +16,68 @@ namespace XFS4IoTFramework.Common
 {
     public sealed class CashPayStatusClass
     {
-        public enum TerminalEnum
+        public class CounterStatusClass
         {
-            Busy,
-            Idle,
-            Inoperative,
-            Unknown
+            public string Currency;
+            public decimal Amount;
+            public int Count;
         }
 
-        public CashPayStatusClass(TerminalEnum Terminal = TerminalEnum.Unknown, string TerminalErrorDescription = "")
+        public enum Availability
         {
-            this.Terminal = Terminal;
-            this.TerminalErrorDescription = TerminalErrorDescription;
+            Unknown,
+            Available,
+            Unavailable,
         }
 
-        public TerminalEnum Terminal { get; set; }
-        public string TerminalErrorDescription { get; set; }
+        public enum FillLevel
+        {
+            Unknown,
+            Normal,
+            Empty,
+            Full,
+            Low
+        }
+
+        public CashPayStatusClass(Availability AcceptCash = Availability.Unknown,
+            Availability Change = Availability.Unknown,
+            FillLevel NoteRecycler = FillLevel.Unknown,
+            FillLevel NoteBin = FillLevel.Unknown,
+            FillLevel CoinHopper1 = FillLevel.Unknown,
+            FillLevel CoinHopper2 = FillLevel.Unknown,
+            FillLevel CoinHopper3 = FillLevel.Unknown,
+            FillLevel CoinHopper4 = FillLevel.Unknown,
+            CounterStatusClass[] NoteBinCounters = null,
+            CounterStatusClass[] NoteRecyclerCounters = null,
+            CounterStatusClass[] CoinHopperCounters = null
+            )
+        {
+            this.AcceptCash = AcceptCash;
+            this.Change = Change;
+            this.NoteRecycler = NoteRecycler;
+            this.NoteBin = NoteBin;
+            this.CoinHopper1 = CoinHopper1;
+            this.CoinHopper2 = CoinHopper2;
+            this.CoinHopper3 = CoinHopper3;
+            this.CoinHopper4 = CoinHopper4;
+            this.NoteBinCounters = NoteBinCounters;
+            this.NoteRecyclerCounters = NoteRecyclerCounters;
+            this.CoinHopperCounters = CoinHopperCounters;
+        }
+
+        public Availability AcceptCash { get; set; }
+        public Availability Change { get; set; }
+
+        public FillLevel NoteRecycler { get; set; }
+        public FillLevel NoteBin { get; set; }
+        public FillLevel CoinHopper1 { get; set; }
+        public FillLevel CoinHopper2 { get; set; }
+        public FillLevel CoinHopper3 { get; set; }
+        public FillLevel CoinHopper4 { get; set; }
+
+
+        public CounterStatusClass[] NoteBinCounters { get; set; }
+        public CounterStatusClass[] NoteRecyclerCounters { get; set; }
+        public CounterStatusClass[] CoinHopperCounters { get; set; }
     }
 }
