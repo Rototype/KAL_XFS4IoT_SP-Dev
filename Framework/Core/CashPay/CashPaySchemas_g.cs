7bbeacec888215f6c97cbc110fd27436f7824355
Claudio Santacesaria <claudio.santacesaria@rototype.com>
2023-01-08 11:40:29 +0100

    StatusClass schemas fix

--- Framework/Core/CashPay/CashPaySchemas_g.cs
+++ Framework/Core/CashPay/CashPaySchemas_g.cs
@@ -14,25 +14,81 @@ namespace XFS4IoT.CashPay
     [DataContract]
     public sealed class StatusClass
     {
-        public StatusClass(TerminalEnum? Terminal = null, string TerminalErrorDescription = "")
+        public StatusClass(Availability AcceptCash = Availability.Unknown,
+            Availability Change = Availability.Unknown,
+            FillLevel NoteRecycler = FillLevel.Unknown,
+            FillLevel NoteBin = FillLevel.Unknown,
+            FillLevel CoinHopper1 = FillLevel.Unknown,
+            FillLevel CoinHopper2 = FillLevel.Unknown,
+            FillLevel CoinHopper3 = FillLevel.Unknown,
+            FillLevel CoinHopper4 = FillLevel.Unknown,
+            CounterStatusClass[] NoteBinCounters = null,
+            CounterStatusClass[] NoteRecyclerCounters = null,
+            CounterStatusClass[] CoinHopperCounters = null
+            )
         {
-            this.Terminal = Terminal;
-            this.TerminalErrorDescription = TerminalErrorDescription;
+            this.AcceptCash = AcceptCash;
+            this.Change = Change;
+            this.NoteRecycler = NoteRecycler;
+            this.NoteBin = NoteBin;
+            this.CoinHopper1 = CoinHopper1;
+            this.CoinHopper2 = CoinHopper2;
+            this.CoinHopper3 = CoinHopper3;
+            this.CoinHopper4 = CoinHopper4;
+            this.NoteBinCounters = NoteBinCounters;
+            this.NoteRecyclerCounters = NoteRecyclerCounters;
+            this.CoinHopperCounters = CoinHopperCounters;
         }
 
-        public enum TerminalEnum
+        public class CounterStatusClass
         {
-            Busy,
-            Idle,
-            Inoperative,
-            Unknown
+            public string Currency;
+            public decimal Amount;
+            public int Count;
         }
 
-        [DataMember(Name = "terminal")]
-        public TerminalEnum? Terminal { get; init; }
+        public enum Availability
+        {
+            Unknown,
+            Available,
+            Unavailable,
+        }
 
-        [DataMember(Name = "terminalErrorDescription")]
-        public string TerminalErrorDescription { get; init; }
+        public enum FillLevel
+        {
+            Unknown,
+            Normal,
+            Empty,
+            Full,
+            Low
+        }
+
+        [DataMember(Name = "acceptCash")]
+        public Availability AcceptCash { get; init; }
+        [DataMember(Name = "Change")]
+        public Availability Change { get; init; }
+
+        [DataMember(Name = "noteRecycler")]
+        public FillLevel NoteRecycler { get; init; }
+        [DataMember(Name = "noteBin")]
+        public FillLevel NoteBin { get; init; }
+        [DataMember(Name = "coinHopper1")]
+        public FillLevel CoinHopper1 { get; init; }
+        [DataMember(Name = "coinHopper2")]
+        public FillLevel CoinHopper2 { get; init; }
+        [DataMember(Name = "coinHopper3")]
+        public FillLevel CoinHopper3 { get; init; }
+        [DataMember(Name = "coinHopper4")]
+        public FillLevel CoinHopper4 { get; init; }
+
+
+        [DataMember(Name = "notBinCounters")]
+        public CounterStatusClass[] NoteBinCounters { get; init; }
+        [DataMember(Name = "noteRecyclerCounters")]
+        public CounterStatusClass[] NoteRecyclerCounters { get; init; }
+        [DataMember(Name = "coinHopperCounters")]
+        public CounterStatusClass[] CoinHopperCounters { get; init; }
+        
     }
 
 
